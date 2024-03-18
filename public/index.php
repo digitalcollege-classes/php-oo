71ec2dca6cbdcfa5c942f8ab8284fc227f3c8759
Marcos <marcosaureliosl@gmail.com>
2024-03-18 13:16:06 -0300

    criação do foreach na logica das routes

--- public/index.php
+++ public/index.php
@@ -1,17 +1,35 @@
 <?php
 
-include "../vendor/autoload.php";
+require_once "../vendor/autoload.php";
+
+use App\Controller\CursoController;
 
 $routes = include "../config/routes.php";
 
 $url = $_SERVER['REQUEST_URI'];
 
-if (false === isset($routes[$url])) {
+$controllerFound = false;
+
+foreach ($routes as $controller => $actions) {
+    if ($controller === CursoController::class && isset($actions['cursos'])) {
+        $prefixo = '/cursos';
+        $rota = substr($url, strlen($prefixo));
+
+        if (isset($actions['cursos'][$rota])) {
+            $controllerFound = true;
+            $method = $actions['cursos'][$rota];
+            (new CursoController())->$method();
+            break;
+        }
+    } elseif (isset($actions[$url])) {
+        $controllerFound = true;
+        $method = $actions[$url];
+        (new $controller())->$method();
+        break;
+    }
+}
+
+if (!$controllerFound) {
     header('location: /erro-404');
     exit;
 }
-
-$controller = $routes[$url][0];
-$method = $routes[$url][1];
-
-(new $controller())->$method();
