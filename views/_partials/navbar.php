5765b5470fab8cd038f5b5be53d11a23d060b89e
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2024-03-28 19:13:12 -0300

    Add button translate

--- views/_partials/navbar.php
+++ views/_partials/navbar.php
@@ -1,19 +1,21 @@
 <hr>
 
-<nav>
-    <a href="/" class="btn btn-outline-dark"><?php echo translate('menu-home'); ?></a>
-    <a href="/professores/listar" class="btn btn-outline-dark"><?= translate('menu-teachers'); ?></a>
-    <a href="/alunos/listar" class="btn btn-outline-dark"><?= translate('menu-students'); ?></a>
-    <a href="/cursos/listar" class="btn btn-outline-dark"><?= translate('menu-courses'); ?></a>
-</nav>
-
-<hr>
 <div class="row">
     <div class="col">
-        Menu
+        <nav>
+            <a href="/" class="btn btn-outline-dark"><?php echo translate('menu-home'); ?></a>
+            <a href="/professores/listar" class="btn btn-outline-dark"><?= translate('menu-teachers'); ?></a>
+            <a href="/alunos/listar" class="btn btn-outline-dark"><?= translate('menu-students'); ?></a>
+            <a href="/cursos/listar" class="btn btn-outline-dark"><?= translate('menu-courses'); ?></a>
+        </nav>
     </div>
-    <div class="col text-end">
-        Idioma
+    <div class="col">
+        <form>
+            <select name="" id="" class="form-select" style="float: right; width: 100px;">
+                <option value="pt-br">PT-BR</option>
+                <option value="en">EN</option>
+            </select>
+        </form>
     </div>
 </div>
 
