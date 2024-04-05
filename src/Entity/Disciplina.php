7dbf57123799a8301fafeab753bd59aa1913ecf1
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2024-04-04 21:11:46 -0300

    Gerando entity disciplina

--- src/Entity/Disciplina.php
+++ src/Entity/Disciplina.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity] #[Table(name: 'subject')]
 class Disciplina
 {
+    #[Id] #[GeneratedValue] #[Column(type: 'integer')]
+    public int $id;
+
+    #[Column(length: 30)]
     public string $nome;
+
+    #[Column(type: 'integer')]
     public int $cargaHoraria;
 }
