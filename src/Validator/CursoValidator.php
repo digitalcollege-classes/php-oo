cc7fee8c3a41315a76f0bfa1176320f777a04038
TalysonSoares <talysonsoares40@gmail.com>
2024-04-11 19:41:14 -0300

    refinando código

--- src/Validator/CursoValidator.php
+++ src/Validator/CursoValidator.php
@@ -10,16 +10,8 @@ class CursoValidator implements Validator
     {
         $errors = [];
 
-        $outputName = strip_tags($_POST['name']);
-        $outputDescription = strip_tags($_POST['description']);
-
-        if ($outputName !== $_POST['name']) {
-            $errors[] = "Erro, caracteres inválidos no nome";
-        }
-
-        if ($outputDescription !== $_POST['description']) {
-            $errors[] = "Erro, caracteres inválidos na descrição";
-        }
+        $_POST['name'] = strip_tags($_POST['name']);
+        $_POST['description'] = strip_tags($_POST['description']);
 
         if (0 === strlen(trim($_POST['name']))) {
             $errors[] = "Erro, nome invalido";
