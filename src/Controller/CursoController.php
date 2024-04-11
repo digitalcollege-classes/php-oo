aae262fe82daeb8e256e7c1638f9d5729b779a94
Paulo Victor <dev.paulovictor@gmail.com>
2024-04-11 20:53:45 -0300

    Remove codigo nao utilizado

--- src/Controller/CursoController.php
+++ src/Controller/CursoController.php
@@ -73,13 +73,4 @@ final class CursoController extends AbstractController
 
         header('location: /cursos/listar');
     }
-
-    public function apiListar(): void
-    {
-        $cursoRepository = $this->entityManager->getRepository(Curso::class);
-        
-        header('Content-Type: application/json; charset=utf-8');
-        http_response_code(200);
-        echo json_encode($cursoRepository->findAll());
-    }
 }
